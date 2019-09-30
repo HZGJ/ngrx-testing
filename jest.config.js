92e94f1b0fdebea2866027ba14641b1075578dd9
GuanglongDu <duguanglong002@163.com>
2019-09-30 10:38:55 +0800

    added json-summary

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   moduleNameMapper: {
     '@core/(.*)': '<rootDir>/src/app/core/$1',
     '@state/(.*)': '<rootDir>/src/app/state/$1'
-  }
+  },
+  coverageReporters: ['json', 'text', 'json-summary' ]
 };
